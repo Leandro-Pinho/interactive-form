df6ec272aa16656c75ba7b4f2dfac27ac701a1b7
Leandro-Pinho <leandroessencia@gmail.com>
2024-07-25 10:37:15 -0300

    adicionando as funções e formulario para front-end

--- script.js
+++ script.js
@@ -10,6 +10,18 @@ function opentab(tabname) {
         tabcontent.classList.remove("active-tab");
     }
 
-    // event.currentTarget.classList.add("active-link");
+    // event.currentTarget.classList.add("active-tab");
     document.getElementById(tabname).classList.add("active-link")
+    // document.getElementById(tabname).classList.add("active-tab")
+}
+
+function openCL(tabname) {
+    
+    for (tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+
+    event.currentTarget.classList.add("active-tab");
+    // document.getElementById(tabname).classList.add("active-link")
+    document.getElementById(tabname).classList.add("active-tab")
 }
