7a04206631868a372e5ca35022efc8e78745eb3a
Leandro-Pinho <leandroessencia@gmail.com>
2024-07-30 10:39:05 -0300

    otimizando o css

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
 /* função para altenar os tab-content, fazendo aparecer no mesmo lugar */
-var tablinks = document.getElementsByClassName("tab-links")
-var tabcontents = document.getElementsByClassName("tab-contents")
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
 
 function opentab(tabname) {
-    for (tablink of tablinks) {
-        tablink.classList.remove("active-link");
-    }
-    for (tabcontent of tabcontents) {
-        tabcontent.classList.remove("active-tab");
-    }
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
 
-    // event.currentTarget.classList.add("active-tab");
-    document.getElementById(tabname).classList.add("active-link")
-    // document.getElementById(tabname).classList.add("active-tab")
+  document.getElementById(tabname).classList.add("active-link");
+  
 }
 
 function openCL(tabname) {
-    
-    for (tabcontent of tabcontents) {
-        tabcontent.classList.remove("active-tab");
-    }
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
 
-    event.currentTarget.classList.add("active-tab");
-    // document.getElementById(tabname).classList.add("active-link")
-    document.getElementById(tabname).classList.add("active-tab")
+  document.getElementById(tabname).classList.add("active-tab");
+}
+
+const sections = document.getElementsByClassName("my-section");
+
+function openCL1(tabname) {
+  for (section of sections) {
+    section.classList.remove("visible-section");
+  }
+  document.getElementById(tabname).classList.add("visible-section");
+}
+
+function openCL2(tabname) {
+  document.getElementById(tabname).classList.remove("visible-section");
 }
